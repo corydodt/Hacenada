62d0596f2f7b69121e3b98bb9d90b56e55ed7c47
Cory Dodt <121705+corydodt@users.noreply.github.com>
2021-01-14 19:40:00 -0800

    meta: releasing 0.1.1

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Hacenada
 Write do-nothing scripts in a declarative (ini) syntax
+
+
+
+
+
+## Change Log
+
+### [0.1.1] - 2021.01.14
+  - First numbered release.
+  - Current features:
+    - Parse a toml script (only type=description and type=message for now)
+    - Display questions and save answers in a tinydb
+    - Recover state between runs, or start over
+    - After the last step, write a log with what happened
+    - Markdown, TOML and JSON print output of the script+answers
+    - Python 3.7, 3.8, 3.9 support
+
+[0.1.1]: https://github.com/corydodt/Codado/tree/v0.1.1
--- setup.py
+++ setup.py
@@ -1,25 +1,20 @@
 from setuptools import find_packages, setup
 
 
-VERSION = "0.0.1+dev"
+VERSION = "0.1.1"
 
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 data = {
     "name": "Hacenada",
-    "version": VERSION,
-    "description": "Write do-nothing scripts",
-    "packages": find_packages(where="./src", include=("hacenada", "hacenada.*")),
-    "package_dir": {"": "src"},
-    "install_requires": [
-        "attrs",
-        "click",
-        "codado",
-        "tinydb-serialization",
-        "typing_extensions; python_version < '3.8'",
-        "pyinquirer",
-        "tinydb",
-        "toml",
+    "classifiers": [
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
     ],
+    "description": "Write do-nothing scripts",
     "extras_require": {
         "dev": [
             "isort",
@@ -31,7 +26,23 @@ data = {
             "black",
         ]
     },
+    "install_requires": [
+        "attrs",
+        "click",
+        "codado",
+        "tinydb-serialization",
+        "typing_extensions; python_version < '3.8'",
+        "pyinquirer",
+        "tinydb",
+        "toml",
+    ],
+    "long_description": long_description,
+    "long_description_content_type": "text/markdown",
+    "packages": find_packages(where="./src", include=("hacenada", "hacenada.*")),
+    "package_dir": {"": "src"},
     "scripts": ["bin/hacenada"],
+    "url": "https://github.com/corydodt/Hacenada",
+    "version": VERSION,
 }
 
 setup(**data)
